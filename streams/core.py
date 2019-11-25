06a430aa5dc45a273aecd5ee29b17ae787c54366
J Samuel Thampy <samjiks@hotmail.com>
2019-11-25 09:13:52 +0000

    Update docs

--- streams/core.py
+++ streams/core.py
@@ -3,7 +3,11 @@ from typing import List, Mapping
 
 class Stream:
     """
-    Stream of data from hetergeneous lists
+    Stream of data from homogeneous lists
+
+    Streams are built on previous functions passing data to
+    lower functions connecting the lower functions to get 
+    refined outputs
     """
 
     def __init__(self, ls):
@@ -11,16 +15,20 @@ class Stream:
 
     @classmethod
     def of(cls, ls: List):
+        """ Provide a class instance way of taking lists of data """
         return cls(ls)
 
     def map(self, func: Mapping):
+        """ Map the data from a list of data"""
         self._ls = list(map(func, self._ls))
         return self
 
     def filter(self, func: Mapping):
+        """Filter functions"""
         self._ls = list(filter(func, self._ls))
         return self
 
     def collect(self, func):
+        """ Collect and output the lists """
         func(self._ls)
         return self._ls
