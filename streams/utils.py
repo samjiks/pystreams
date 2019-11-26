823f6bfd8a7e8ef9e72220e0098e18cc89fbc9ba
J Samuel Thampy <samjiks@hotmail.com>
2019-11-26 13:06:14 +0000

    Move functions out of streams

--- streams/utils.py
+++ streams/utils.py
@@ -4,12 +4,15 @@ import streams
 
 
 def map_list(func: streams.Stream, ls: List) -> List:
+    """util to help map function from a list"""
     return list(map(func, ls))
 
 
 def filter_list(func: streams.Stream, ls: List) -> List:
+    """util to help filter function from a list"""
     return list(filter(func, ls))
 
 
 def is_list(ls: List) -> bool:
+    """ check if it is a list """
     return isinstance(ls, list)
